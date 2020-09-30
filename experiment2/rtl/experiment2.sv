1e3f550d6c56dc978aa4537de536020e97385c69
j-fw <70967913+j-fw@users.noreply.github.com>
2020-09-30 15:41:40 -0400

    drawing multiple rectangles on screen

--- experiment2/rtl/experiment2.sv
+++ experiment2/rtl/experiment2.sv
@@ -38,6 +38,12 @@ logic [9:0] pixel_X_pos;
 logic [9:0] pixel_Y_pos;
 
 logic object_on;
+logic object_on1;
+logic object_on2;
+logic object_on3;
+logic object_on4;
+logic object_on5;
+logic object_on6;
 
 assign resetn = ~SWITCH_I[17];
 
@@ -77,11 +83,30 @@ end
 // and line counter is between rows 220 and 259 (inclusive)
 // assert the "object_on" signal
 always_comb begin
-	if (pixel_X_pos >= 10'd300 && pixel_X_pos < 10'd340
-	 && pixel_Y_pos >= 10'd220 && pixel_Y_pos < 10'd260) 
+
+	object_on = 1'b0;
+	object_on1 = 1'b0;
+	object_on2 = 1'b0;
+	object_on3 = 1'b0;
+	object_on4 = 1'b0;
+	object_on5 = 1'b0;
+	object_on6 = 1'b0;
+	
+	if (pixel_X_pos >= 10'd300 && pixel_X_pos < 10'd340 && pixel_Y_pos >= 10'd220 && pixel_Y_pos < 10'd260) 
 		object_on = 1'b1;
-	else 
-		object_on = 1'b0;
+	else if (pixel_X_pos >= 10'd300 && pixel_X_pos < 10'd340 && pixel_Y_pos >= 10'd320 && pixel_Y_pos < 10'd360) 
+		object_on1 = 1'b1;
+	else if (pixel_X_pos >= 10'd10 && pixel_X_pos < 10'd50 && pixel_Y_pos >= 10'd220 && pixel_Y_pos < 10'd260) 
+		object_on2 = 1'b1;
+	else if (pixel_X_pos >= 10'd100 && pixel_X_pos < 10'd140 && pixel_Y_pos >= 10'd220 && pixel_Y_pos < 10'd260) 
+		object_on3 = 1'b1;
+	else if (pixel_X_pos >= 10'd300 && pixel_X_pos < 10'd340 && pixel_Y_pos >= 10'd400 && pixel_Y_pos < 10'd440) 
+		object_on4 = 1'b1;
+	else if (pixel_X_pos >= 10'd660 && pixel_X_pos < 10'd640 && pixel_Y_pos >= 10'd220 && pixel_Y_pos < 10'd260) 
+		object_on5 = 1'b1;
+	else if (pixel_X_pos >= 10'd300 && pixel_X_pos < 10'd380 && pixel_Y_pos >= 10'd110 && pixel_Y_pos < 10'd120) 
+		object_on6 = 1'b1;
+		
 end
 
 // the background is black and a white square is 
@@ -90,12 +115,35 @@ always_comb begin
 	VGA_red = 8'h00;
 	VGA_green = 8'h00;
 	VGA_blue = 8'h00;
-	if (object_on == 1'b1) 
-		VGA_red = 8'hFF;
-	if (object_on == 1'b1) 
+	if (object_on == 1'b1) begin
+		VGA_red = 8'hFF; 
 		VGA_green = 8'hFF;
-	if (object_on == 1'b1) 
 		VGA_blue = 8'hFF;
+	end else if(object_on1 == 1'b1) begin
+		VGA_red = 8'hFF; 
+		VGA_green = 8'h00;
+		VGA_blue = 8'h00;
+	end else if(object_on2 == 1'b1) begin
+		VGA_red = 8'h00; 
+		VGA_green = 8'hFF;
+		VGA_blue = 8'h00;
+	end else if(object_on3 == 1'b1) begin
+		VGA_red = 8'h0; 
+		VGA_green = 8'h0;
+		VGA_blue = 8'hFF;
+	end else if(object_on4 == 1'b1) begin
+		VGA_red = 8'hFF; 
+		VGA_green = 8'h00;
+		VGA_blue = 8'hFF;
+	end else if(object_on5 == 1'b1) begin
+		VGA_red = 8'hFF; 
+		VGA_green = 8'hFF;
+		VGA_blue = 8'h00;
+	end else if(object_on6 == 1'b1) begin
+		VGA_red = 8'h00; 
+		VGA_green = 8'hFF;
+		VGA_blue = 8'hFF;
+	end
 end
 
 endmodule
